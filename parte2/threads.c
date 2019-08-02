cdd93e81976673b7f89850bdbc6822fe6f4400ae
Invitado <invitado@s5pc7.eisc.univalle.edu.co>
2019-08-02 13:20:31 -0500

    revisión de los resultados parte 2

--- parte2/threads.c
+++ parte2/threads.c
@@ -4,36 +4,40 @@
 #include "common.h"
 #include "common_threads.h"
 #include "mycommon.h"
+puerta puertaP;
 
-volatile int v = 0;
+volatile float v = 0;
 
 void *operacion1(void *arg) {
-	int x=1250;
         int y=350;
         int z=25;
+	cerrar_puerta(puertaP);
 	sleep (2);
-	v=(z/y);
-	printf("Initial value : %d\n", v);
+	v=(float)z/(float)y;
+	printf("v en hilo 1 : %f\n", v);
+	abrir_puerta(puertaP);
     return NULL;
 }
 void *operacion2(void *arg) {
 	int x=1250;
-        int y=350;
-        int z=25;
-        v=x*v;
-	printf("Initial value : %d\n", v);
+	cerrar_puerta(puertaP);
+        v=v*(float)x;
+	printf("v en hilo 2 : %f\n", v);
+	abrir_puerta(puertaP);
     return NULL;
 }
 
 
 int main(int argc, char *argv[]) {
+    crear_puerta(puertaP);
     pthread_t p1, p2;
-    printf("Initial value : %d\n", v);
+    printf("Initial value : %f\n", v);
     Pthread_create(&p1, NULL, operacion1, NULL); 
     Pthread_create(&p2, NULL, operacion2, NULL);
 	Pthread_join(p1, NULL);
     Pthread_join(p2, NULL);
-    printf("Final value   : %d\n", v);
+    printf("Final value   : %f\n", v);
+    destruir_puerta(puertaP);
     return 0;
 }
 
